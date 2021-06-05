b3aea093d2dc7bb4e4ccd053434b0f336a0b1b1b
Deekshith76 <deekshith_b180791ce@nitc.ac.in>
2021-06-05 19:08:21 +0530

    Minion Game

--- demopro.py
+++ demopro.py
@@ -1,1 +1,22 @@
-print("Goodbye World")
+#Minion Game
+
+def minion_game(string):
+    vowels = 'AEIOU'
+    kevin_score, steven_score=0,0
+    for i in range(len(string)):
+        score = len(string)-i
+        if string[i] in vowels:
+            kevin_score += score
+        else:
+            steven_score += score
+    if kevin_score > steven_score:
+        print("Kevin", kevin_score)
+    elif kevin_score < steven_score:
+        print("Steven", steven_score)
+    else:
+        print("Draw")
+        
+string =input(" ")
+minion_game(string)
+    
+    
